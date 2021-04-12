f4c1f4b2637a447a56118bd53321a7f3e811ba80
theiconik <kumarujjawal51@gmail.com>
2021-04-12 12:55:03 +0530

    Understanding click events and parameters

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+   
+   const handleClick = () => {
+      console.log('hello,ninjas');
+   }
+
+   const handleClickAgain = (name,e) => {
+      console.log('hello ' + name, e.target);
+   }
+
    return ( 
       <div className="home">
          <h2>Homepage</h2>
+         <button onClick={handleClick}>Click me</button>
+         <button onClick={(e) => {
+            handleClickAgain('mario', e)
+         }}>Click again</button>
       </div>
     );
 }
