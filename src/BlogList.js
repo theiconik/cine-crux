ed5c21c5374ec2e8962c0926456af00e7a2e36aa
theiconik <kumarujjawal51@gmail.com>
2021-04-12 18:29:03 +0530

    Props and Temporarily Deleting using filter function

---
+++ src/BlogList.js
@@ -0,0 +1,22 @@
+const BlogList = (props) => {
+   
+   const blogs = props.blogprop;
+   const handleDelete = props.handleDeleteprop;
+   return ( 
+      <div className="blog-list">
+         {blogs.map( (blog) => (
+            <div className="blog-preview" key = {blog.id}>
+               <h2>{blog.title}</h2>
+               <p>{blog.body}</p>
+               <p><i><strong style = {
+                  {
+                     color: "#a5a8ac",
+                  }}>by {blog.author}</strong></i></p>
+               <button onClick={() => handleDelete(blog.id)}>Delete</button>
+            </div>
+         ) )}
+      </div>
+    );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,37 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
 
-   //let name = 'mario';
-   const [name, setName] = useState('mario');
-   const [age, setAge] = useState(25)
-   
-   const handleClick = () => {
-      //console.log('hello, ninjas')
-      setName('luigi');
-      setAge(26);
-      //name = luigi;
+   const [blogs, setBlogs] = useState([
+      { title: 'My new Website', 
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      author: 'Mario',
+      id: 1,
+      },
+
+      { title: 'Welcome party', 
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.",
+      author: 'Yoshi',
+      id: 2,
+      },
+
+      { title: 'Have fun', 
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+      author: 'Luigi',
+      id: 3,
+      },
+   ]);
+
+   const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id)
+      setBlogs(newBlogs);
    }
 
-   const handleClickAgain = (name,e) => {
-      console.log('hello ' + name, e.target);
-   }
 
    return ( 
       <div className="home">
-         <h2>Homepage</h2>
-         <p>{name} is {age} years old.</p>
-         <button onClick={handleClick}>Click me</button>
-         <button onClick={(e) => {
-            handleClickAgain('mario', e)
-         }}>Click again</button>
+         <BlogList blogprop={blogs} handleDeleteprop={handleDelete} />
       </div>
     );
 }
