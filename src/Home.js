dfa40ca1a4d4a5f1d76378c9679618d411f9d231
theiconik <kumarujjawal51@gmail.com>
2021-04-13 13:00:44 +0530

    Added loading animation

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import loadinglogo from './Ripple-1s-200px.gif'
 
 const Home = () => {
 
    const [blogs, setBlogs] = useState(null);
+   const [isPending, setIsPending] = useState(true);
 
    // const handleDelete = (id) => {
    //    const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -12,19 +14,23 @@ const Home = () => {
 
    useEffect(() => {
       // console.log("Use Effect ran!");
-      fetch('http://localhost:8000/blogs')
+      setTimeout(() => {
+         fetch('http://localhost:8000/blogs')
       .then(res => {
          return res.json();
       })
       .then((data) => {
          console.log(data);
          setBlogs(data);
+         setIsPending(false);
       })
+      }, 1000);
    }, [])
 
 
    return ( 
       <div className="home">
+         {isPending && <div style={{textAlign: "center"}}><img width="80px" height="80px" src={loadinglogo} alt="yo"></img></div>}
          {blogs && <BlogList blogprop={blogs} handleDeleteprop="handleDelete" />}
       </div>
     );
