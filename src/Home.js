e4c781c096479f766be1ebae23c719440b7a8cf2
theiconik <kumarujjawal51@gmail.com>
2021-04-13 11:28:53 +0530

    useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -27,6 +27,10 @@ const Home = () => {
       const newBlogs = blogs.filter(blog => blog.id !== id)
       setBlogs(newBlogs);
    }
+
+   useEffect(() => {
+      console.log("Use Effect ran!");
+   }, [])
 
 
    return ( 
