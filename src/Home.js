ac3e3707678f3218ef32ce6b119e1d369b8ef711
theiconik <kumarujjawal51@gmail.com>
2021-04-14 12:25:57 +0530

    Handling errors and outputting them

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
 
    const [blogs, setBlogs] = useState(null);
    const [isPending, setIsPending] = useState(true);
+   const [error, setError] = useState(null);
 
    // const handleDelete = (id) => {
    //    const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -17,12 +18,20 @@ const Home = () => {
       setTimeout(() => {
          fetch('http://localhost:8000/blogs')
       .then(res => {
+         if(!res.ok) {
+            throw Error('Could not fetch from the source API !?');
+         }
          return res.json();
       })
       .then((data) => {
-         console.log(data);
+         //console.log(data);
          setBlogs(data);
          setIsPending(false);
+         setError(null);
+      })
+      .catch(err => {
+         setIsPending(false);
+         setError(err.message);
       })
       }, 1000);
    }, [])
@@ -30,6 +39,7 @@ const Home = () => {
 
    return ( 
       <div className="home">
+         {error && <div style={{textAlign:"center", padding:"5px 10px",border:"1px solid red", borderRadius:"8px",}}> {error} </div>}
          {isPending && <div style={{textAlign: "center"}}><img width="80px" height="80px" src={loadinglogo} alt="yo"></img></div>}
          {blogs && <BlogList blogprop={blogs} handleDeleteprop="handleDelete" />}
       </div>
