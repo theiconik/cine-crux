249c495d50049287597d3ccf1d5f90edd408f17a
theiconik <kumarujjawal51@gmail.com>
2021-04-14 21:23:05 +0530

    Added route for creation of blog

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import Navbar from './Components/Navbar'
-import Home from './Routes/Home'
+import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Components/Create";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+   return ( 
+      <div className="create">
+         <h3 style={{color: "#f1356d",textAlign:"center"}}>Add a new blog</h3>
+      </div>
+    );
+}
+ 
+export default Create;
